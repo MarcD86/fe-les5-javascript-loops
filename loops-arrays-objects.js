e8772f8f46b98c9587499a7def32e64c8517d601
MarcD86 <marc_duim@live.nl>
2023-03-27 20:45:32 +0200

    update1

--- loops-arrays-objects.js
+++ loops-arrays-objects.js
@@ -9,6 +9,10 @@ const scores = [
     {name: 'Rianne', score: 66, grade: null}
 ];
 
+// for (let i = 0; i < scores.length; i++) {
+//     console.log(scores[i].score)
+// }
+
 // Verwachtte uitkomsten:
 // 83
 // 77
@@ -32,6 +36,24 @@ const scores = [
 // D
 // ==========================================
 
+// for (let i = 0; i < scores.length; i++) {
+//
+//     if (scores[i].score < 60 ) {
+//         console.log('F')
+//     }
+//     if (scores[i].score > 60 && scores[i].score < 70) {
+//         console.log('D')
+//     }
+//     if (scores[i].score > 70 && scores[i].score < 80 ) {
+//         console.log('C')
+//     }
+//     if (scores[i].score > 80 && scores[i].score < 90 ) {
+//         console.log('B')
+//     }
+//     if (scores[i].score > 90 && scores[i].score < 100 ) {
+//         console.log('A')
+//     }
+// }
 
 
 
@@ -49,7 +71,29 @@ const scores = [
 //  ];
 // ==========================================
 
+for (let i = 0; i < scores.length; i++) {
 
+    if (scores[i].score < 60 ) {
+        scores[i].grade = 'F'
+        console.log(scores[i])
+    }
+    if (scores[i].score > 60 && scores[i].score < 70) {
+        scores[i].grade = 'D'
+        console.log(scores[i])
+    }
+    if (scores[i].score > 70 && scores[i].score < 80 ) {
+        scores[i].grade = 'C'
+        console.log(scores[i])
+    }
+    if (scores[i].score > 80 && scores[i].score < 90 ) {
+        scores[i].grade = 'B'
+        console.log(scores[i])
+    }
+    if (scores[i].score > 90 && scores[i].score < 100 ) {
+        scores[i].grade = 'A'
+        console.log(scores[i])
+    }
+}
 
 
 
@@ -57,12 +101,17 @@ const scores = [
 // Opdracht 2
 // Schrijf een script die e-mailadressen genereert voor al onze medewerkers. Sla dit op in een nieuwe property "email" die je toevoegt aan iedere medewerker.
 
-const NOVIEmployees = [
-    {firstName: 'Nova', lastName: 'Eeken'},
-    {firstName: 'Sam', lastName: 'Barnhoorn'},
-    {firstName: 'Tessa', lastName: 'Steur'},
-    {firstName: 'Mark', lastName: 'Rensen'},
-];
+// const NOVIEmployees = [
+//     {firstName: 'Nova', lastName: 'Eeken'},
+//     {firstName: 'Sam', lastName: 'Barnhoorn'},
+//     {firstName: 'Tessa', lastName: 'Steur'},
+//     {firstName: 'Mark', lastName: 'Rensen'},
+// ];
+//
+// for (let i = 0; i < NOVIEmployees.length; i++) {
+//     NOVIEmployees[i].email = NOVIEmployees[i].firstName + '.' + NOVIEmployees[i].lastName + '@novi.nl';
+//     console.log(NOVIEmployees[i])
+// }
 
 // Bij NOVI horen de e-mailadressen altijd in het volgende format: voornaam.achternaam@novi.nl
 // Vóór het script zie je de originele objecten,
@@ -84,7 +133,17 @@ const NOVIEmployees = [
 // Lukt het je om ervoor te zorgen dat alle e-mailadressen in lowercase letters komen te staan? Dit heb je nog niet geleerd, maar google is your best friend...
 // ==========================================
 
+const NOVIEmployees = [
+    {firstName: 'Nova', lastName: 'Eeken'},
+    {firstName: 'Sam', lastName: 'Barnhoorn'},
+    {firstName: 'Tessa', lastName: 'Steur'},
+    {firstName: 'Mark', lastName: 'Rensen'},
+];
 
+for (let i = 0; i < NOVIEmployees.length; i++) {
+    NOVIEmployees[i].email = NOVIEmployees[i].firstName.toLowerCase() + '.' + NOVIEmployees[i].lastName.toLowerCase() + '@novi.nl';
+    console.log(NOVIEmployees[i])
+}
 
 
 // ==========================================
@@ -113,6 +172,40 @@ const students = [
     {name: 'Aicha', city: 'Utrecht', zipCode: '3514', neighborhood: null},
     {name: 'Karima', city: 'Utrecht', zipCode: '3531', neighborhood: null},
 ];
+
+for (let i = 0; i < students.length; i++) {
+    if (students[i].zipCode === '3513') {
+        students[i].neighborhood = students[i].neighborhood = 'Pijlsweerd';
+        console.log(students[i])
+    }
+    if (students[i].zipCode === '3514') {
+        students[i].neighborhood = students[i].neighborhood = 'Vogelenbuurt';
+        console.log(students[i])
+    }
+    if (students[i].zipCode === '3512') {
+        students[i].neighborhood = students[i].neighborhood = 'Binnenstad';
+        console.log(students[i])
+    }
+    if (students[i].zipCode === '3531') {
+        students[i].neighborhood = students[i].neighborhood = 'Lombok';
+        console.log(students[i])
+    }
+    if (students[i].zipCode === '3572') {
+        students[i].neighborhood = students[i].neighborhood = 'Wittevrouwen';
+        console.log(students[i])
+    }
+    if (students[i].zipCode === '3581') {
+        students[i].neighborhood = students[i].neighborhood = 'Oudwijk';
+        console.log(students[i])
+    }
+    if (students[i].zipCode === '3583') {
+        students[i].neighborhood = students[i].neighborhood = 'Schildersbuurt';
+        console.log(students[i])
+    }
+// verwachtte uitkomsten klopt niet helemaal ... alles is vogelenbuurt :p
+}
+
+
 
 // Verwachte uitkomsten:
 // [
